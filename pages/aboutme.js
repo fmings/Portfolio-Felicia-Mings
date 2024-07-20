e664e343ab1faf8dab4c03c9a2fc2f3173064c3e
fmings <mingsfelicia@gmail.com>
2024-07-20 12:24:36 -0500

    set up initial files

---
+++ pages/aboutme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutMeCard from '../components/AboutMeCard';
+
+export default function aboutme() {
+  return (
+    <div>
+      <AboutMeCard />
+    </div>
+  );
+}
---
+++ pages/myprojects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+
+export default function myprojects() {
+  return (
+    <div>
+      <ProjectCard />
+    </div>
+  );
+}
