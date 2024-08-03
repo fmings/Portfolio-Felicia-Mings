603d3029e5185022ea85b4672f47e1596f06025c
fmings <mingsfelicia@gmail.com>
2024-08-03 10:27:47 -0500

    added home page content

--- components/HomeAboutMeCard.js
+++ components/HomeAboutMeCard.js
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default function HomeAboutMeCard() {
   return (
-    <div>
-      Home Page About Me
+    <div className="homepage-wrapper">
+      <div className="about-me-intro-block">
+        <div className="headshot-photo">Insert Photo Here</div>
+        <div className="intro-headline">Hello, I&apos;m Felicia</div>
+        <div className="about-me-intro">
+          I am a multi-passionate individual with professional experience in Merchandising, Marketing, Project Management, Communications, and Web Development. I am currently learning to code at Nashville Software School. Explore my portfolio to see examples showcasing my design and coding skills!
+        </div>
+      </div>
+      <div className="navigation-blocks" />
+      <div className="navigation-block-button"><Link passHref href="/aboutme"><Button>About Me</Button></Link></div>
+      <div className="navigation-block-button"><Link passHref href="/myresume"><Button>My Resume</Button></Link></div>
+      <div className="navigation-block-button"><Link pssHref href="myprojects"><Button>My Projects</Button></Link></div>
     </div>
+
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
+import HomeAboutMeCard from '../components/HomeAboutMeCard';
+
 function Home() {
   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -16,7 +18,7 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      <HomeAboutMeCard />
     </div>
   );
 }
